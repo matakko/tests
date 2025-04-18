990e9539082b20d4593e4a2fac455a3cbdd0e1a0
Matako <82229906+matakko@users.noreply.github.com>
2025-04-18 21:10:02 +0700

    Update emuDeckXenia.sh

--- xeniappimage/final/emuDeckXenia.sh
+++ xeniappimage/final/emuDeckXenia.sh
@@ -2,11 +2,13 @@
 #variables
 Xenia_emuName="Xenia"
 Xenia_emuType="$emuDeckEmuTypeBinary"
-Xenia_emuPath="$emusFolder/xenia_canary"
+Xenia_emuPath="$emusFolder"
 Xenia_releaseURL_canary="https://github.com/xenia-canary/xenia-canary-releases/releases/latest/download/xenia_canary_linux.tar.gz"
-Xenia_XeniaSettings="$Xenia_emuPath/xenia-canary.config.toml"
-#TODOlist: make decisions towards folder strcuture, Uninstall, ESDEconf, add patches, cleanstuff + check controls / compa to mix proton install
-#cleanupOlderThings
+Xenia_dir="$HOME/.local/share/Xenia"
+Xenia_XeniaSettings="$$HOME/.local/share/Xenia/xenia-canary.config.toml"
+# check TODO for find stuff todo or to finish
+
+#cleanupOlderThings TODO
 Xenia_cleanup(){
 	echo "NYI"
 }
@@ -19,9 +21,10 @@ Xenia_install(){
     local archiveName="xenia_canary_linux"
 
     if installEmuBI "$Xenia_emuName" "$Xenia_releaseURL_canary" "$archiveName" "tar.gz" "$showProgress"; then
-        mkdir -p "$Xenia_emuPath"
+        mkdir -p "$Xenia_emuPath/content"
         tar -xvf "$emusFolder/${archiveName}.tar.gz" -C "$Xenia_emuPath"
         rm -f "$emusFolder/${archiveName}.tar.gz"
+        mv "$Xenia_emuPath/LICENSE" "$Xenia_dir"
         chmod +x "$Xenia_emuPath/xenia_canary"
     else
         return 1
@@ -32,22 +35,26 @@ Xenia_install(){
 Xenia_init(){
 	echo "Initializing Xenia Config"
 
-    mkdir -p "$romsPath/xbox360/roms/xbla"
-    configEmuAI "Xenia" "config" "$Xenia_emuPath" "$emudeckBackend/configs/xenia/xenia-canary.config.toml" "true"
+    mkdir -p "$romsPath/xbox360/roms/xbla" #doest it need to be symlink to local/share/xenia  ? TODO
+    configEmuAI "Xenia" "config" "$Xenia_dir" "$emudeckBackend/configs/xenia" "true"
 
-#    Xenia_addESConfig TODO(need to add system , find rules wil be achieve via laucnher ?)
-#    Xenia_setupStorage TODO(pehaps modfiy path patches ? )
+#    Xenia_setupStorage TODO (pehaps modfiy path patches ? )
     Xenia_setupSaves
     Xenia_getPatches
-#    Xenia_finalize TODO(Perhaps add cleanstuff)
-	#SRM_createParsers
+#    Xenia_finalize TODO check if we need to add some clean
   Xenia_flushEmulatorLauncher
+	if [ -e "$ESDE_toolPath" ] || [ -f "${toolsPath}/$ESDE_downloadedToolName" ] || [ -f "${toolsPath}/$ESDE_oldtoolName.AppImage" ]; then
+		Xenia_addESConfig
+	else
+		echo "ES-DE not found. Skipped adding custom system."
+	fi
 }
 
 #update
 Xenia_update(){
 	echo "NYI"
 	Xenia_setupSaves
+#	Xenia_getPatches  TODO verify if its ok to do 
 	Xenia_flushEmulatorLauncher
 }
 
@@ -57,50 +64,52 @@ Xenia_setEmulationFolder(){
 }
 
 #ConfigureESDE
-#Xenia_addESConfig(){
-#
-#	ESDE_junksettingsFile
-#	ESDE_addCustomSystemsFile
-#	ESDE_setEmulationFolder
-#
-#	if [[ $(grep -rnw "$es_systemsFile" -e 'xbox360') == "" ]]; then
-#		xmlstarlet ed -S --inplace --subnode '/systemList' --type elem --name 'system' \
-#		--var newSystem '$prev' \
-#		--subnode '$newSystem' --type elem --name 'name' -v 'xbox360' \
-#		--subnode '$newSystem' --type elem --name 'fullname' -v 'Microsoft Xbox 360' \
-#		--subnode '$newSystem' --type elem --name 'path' -v '%ROMPATH%/xbox360/roms' \
-#		--subnode '$newSystem' --type elem --name 'extension' -v '.iso .ISO . .xex .XEX' \
+Xenia_addESConfig(){
+
+	ESDE_junksettingsFile
+	ESDE_addCustomSystemsFile
+	ESDE_setEmulationFolder
+
+	if [[ $(grep -rnw "$es_systemsFile" -e 'xbox360') == "" ]]; then
+		xmlstarlet ed -S --inplace --subnode '/systemList' --type elem --name 'system' \
+		--var newSystem '$prev' \
+		--subnode '$newSystem' --type elem --name 'name' -v 'xbox360' \
+		--subnode '$newSystem' --type elem --name 'fullname' -v 'Microsoft Xbox 360' \
+		--subnode '$newSystem' --type elem --name 'path' -v '%ROMPATH%/xbox360/roms' \
+		--subnode '$newSystem' --type elem --name 'extension' -v '.iso .ISO . .xex .XEX' \
 #		--subnode '$newSystem' --type elem --name 'commandP' -v "/bin/bash ${toolsPath}/launchers/xenia.sh z:%ROM% %INJECT%=%BASENAME%.esprefix" \
+		--subnode '$newSystem' --type elem --name 'commandP' -v "%EMULATOR_XENIA% %ROM%" \
 #		--insert '$newSystem/commandP' --type attr --name 'label' --value "Xenia (Proton)" \
-#		--subnode '$newSystem' --type elem --name 'platform' -v 'xbox360' \
-#		--subnode '$newSystem' --type elem --name 'theme' -v 'xbox360' \
-#		-r 'systemList/system/commandP' -v 'command' \
-#		"$es_systemsFile"
-#
+		--insert '$newSystem/commandP' --type attr --name 'label' --value "Xenia Canary (Linux)" \
+		--subnode '$newSystem' --type elem --name 'platform' -v 'xbox360' \
+		--subnode '$newSystem' --type elem --name 'theme' -v 'xbox360' \
+		-r 'systemList/system/commandP' -v 'command' \
+		"$es_systemsFile"
+
 		#format doc to make it look nice
-#		xmlstarlet fo "$es_systemsFile" > "$es_systemsFile".tmp && mv "$es_systemsFile".tmp "$es_systemsFile"
-#	fi
+		xmlstarlet fo "$es_systemsFile" > "$es_systemsFile".tmp && mv "$es_systemsFile".tmp "$es_systemsFile"
+	fi
 	#Custom Systems config end
-#}
+}
 #SetupPatches
 Xenia_getPatches() {
 	local patches_url="https://github.com/xenia-canary/game-patches/releases/latest/download/game-patches.zip"
 
-	mkdir -p "$Xenia_emuPath/patches"
-	if  [[ ! "$( ls -A "$Xenia_emuPath/patches")" ]] ; then
-		{ curl -L "$patches_url" -o "$Xenia_emuPath/game-patches.zip" && nice -n 5 unzip -q -o "$Xenia_emuPath/game-patches.zip" -d "$Xenia_emuPath" && rm "$Xenia_emuPath/game-patches.zip"; } &> /dev/null
+	mkdir -p "$Xenia_dir/patches"
+	if  [[ ! "$( ls -A "$Xenia_dir/patches")" ]] ; then
+		{ curl -L "$patches_url" -o "$Xenia_dir/game-patches.zip" && nice -n 5 unzip -q -o "$Xenia_dir/game-patches.zip" -d "$Xenia_dir" && rm "$Xenia_dir/game-patches.zip"; } &> /dev/null
 		echo "Xenia patches downloaded."
 	else
-		{ curl -L "$patches_url" -o "$Xenia_emuPath/game-patches.zip" && nice -n 5 unzip -uqo "$Xenia_emuPath/game-patches.zip" -d "$Xenia_emuPath" && rm "$Xenia_emuPath/game-patches.zip"; } &> /dev/null
+		{ curl -L "$patches_url" -o "$Xenia_dir/game-patches.zip" && nice -n 5 unzip -uqo "$Xenia_dir/game-patches.zip" -d "$Xenia_dir" && rm "$Xenia_dir/game-patches.zip"; } &> /dev/null
 		echo "Xenia patches updated."
 	fi
-#	linkToSaveFolder xenia patches "$Xenia_emuPath/patches" Maybe add it to storage ? 
+#	linkTo.... xenia STORAGE? "$Xenia_dir/patches" TODO
 }
 
 #SetupSaves
 Xenia_setupSaves(){
-	mkdir -p "$Xenia_emuPath/content"
-#	linkToSaveFolder xenia saves "$Xenia_emuPath/content"
+	mkdir -p "$Xenia_dir/content"
+#	linkToSaveFolder xenia saves "$Xenia_dir/content" TODO
 }
 
 
@@ -119,6 +128,7 @@ Xenia_wipeSettings(){
 #Uninstall
 Xenia_uninstall(){
 	echo "NYI"
+# TODO idk what to remove for now a part local/share/xenia and Applications/xenia_canary . launcher maybe ?
 #	setMSG "Uninstalling $Xenia_emuName. Saves and ROMs will be retained in the ROMs folder."
 #	find ${romsPath}/xbox360 -mindepth 1 \( -name roms -o -name content \) -prune -o -exec rm -rf '{}' \; &>> /dev/null
 #	rm -rf $HOME/.local/share/applications/xenia.desktop &> /dev/null
